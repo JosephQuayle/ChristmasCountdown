759eb84c6044003b7b918ae8d818e408a9753620
JosephQuayle <72473511+JosephQuayle@users.noreply.github.com>
2022-02-16 17:37:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //date we are counting to
-const christmasDay = new Date("December 25, 2021 00:00:00").getTime();
+const christmasDay = new Date("December 25, 2022 00:00:00").getTime();
 
 //update every second
 let x = setInterval(function(){
